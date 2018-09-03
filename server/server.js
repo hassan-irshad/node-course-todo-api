40e72c132d9a83a52cd9385026d8ea72cfaa775f
Hassan Irshad <hassanansari17@live.com>
2018-09-03 14:08:26 +0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos', (req, res) => {
     
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
 app.listen(5000, () => {
     console.log('Started on port 5000');
 });
