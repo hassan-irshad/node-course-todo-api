521f09cffd0ccda1110faf34cb5521a7e593c82f
Hassan Irshad <hassanansari17@live.com>
2018-09-04 14:44:31 +0500

    Add delete todos by id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,9 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todos');
+const {User} = require('./../server/models/users');
+
+Todo.remove({}).then((result) => {
+    console.log(result);
+});
